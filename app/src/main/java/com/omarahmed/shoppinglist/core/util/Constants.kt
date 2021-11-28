4695c219d3b3d156acfa0d7b01046da19e4ef6b0
om2013ab <60521259+om2013ab@users.noreply.github.com>
2021-11-28 21:08:38 +0800

    Add swipeRefresh layout

--- app/src/main/java/com/omarahmed/shoppinglist/core/util/Constants.kt
+++ app/src/main/java/com/omarahmed/shoppinglist/core/util/Constants.kt
@@ -2,7 +2,7 @@ package com.omarahmed.shoppinglist.core.util
 
 object Constants {
 
-    const val MAX_ITEM_NAME_LENGTH = 10
+    const val MAX_ITEM_NAME_LENGTH = 15
     const val PAGE_SIZE = 10
     const val BASE_URL = "http://192.168.0.125:1996/"
     const val ITEMS_TABLE = "shopping_items"
--- app/src/main/java/com/omarahmed/shoppinglist/feature_list/presentation/screen_home/HomeScreen.kt
+++ app/src/main/java/com/omarahmed/shoppinglist/feature_list/presentation/screen_home/HomeScreen.kt
@@ -15,6 +15,8 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.annotation.ExperimentalCoilApi
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.omarahmed.shoppinglist.feature_list.presentation.screen_home.components.ShoppingItem
 import com.omarahmed.shoppinglist.core.presentation.ui.theme.*
 import com.omarahmed.shoppinglist.feature_cart.data.entity.CartEntity
@@ -37,19 +39,38 @@ fun HomeScreen(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        LazyVerticalGrid(
-            cells = GridCells.Fixed(2),
-            contentPadding = PaddingValues(
-                bottom = SuperLargeSpace,
-                start = SmallSpace,
-                end = SmallSpace,
-                top = SmallSpace
-            ),
+        SwipeRefresh(
+            state = rememberSwipeRefreshState(isRefreshing = allItems.loadState.refresh is LoadState.Loading),
+            onRefresh = {allItems.refresh()},
+            modifier = Modifier.fillMaxSize()
         ) {
-            items(allItems.itemCount) { index ->
-                allItems[index]?.let {item ->
-                    ShoppingItem(shoppingItem = item) {
-                        if (!item.isAddedToCart){
+            LazyVerticalGrid(
+                cells = GridCells.Fixed(2),
+                contentPadding = PaddingValues(
+                    bottom = SuperLargeSpace,
+                    start = SmallSpace,
+                    end = SmallSpace,
+                    top = SmallSpace
+                ),
+            ) {
+                items(allItems.itemCount) { index ->
+                    allItems[index]?.let {item ->
+                        ShoppingItem(shoppingItem = item) {
+                            homeViewModel.updateItem(item)
+                            if (item.isAddedToCart){
+                                cartViewModel.insertItem(
+                                    CartEntity(
+                                        itemName = item.name,
+                                        itemIconUrl = item.imageUrl ?: "",
+                                        itemId = item.id
+                                    )
+                                )
+
+                            } else {
+                                cartViewModel.deleteItem(item.id)
+                            }
+                        }
+                        if (item.isAddedToCart){
                             cartViewModel.insertItem(
                                 CartEntity(
                                     itemName = item.name,
@@ -57,29 +78,19 @@ fun HomeScreen(
                                     itemId = item.id
                                 )
                             )
+
                         } else {
                             cartViewModel.deleteItem(item.id)
                         }
-
-                        homeViewModel.updateItem(item)
                     }
                 }
-            }
 
-        }
-        when {
-            allItems.loadState.refresh is LoadState.Loading -> {
-                CircularProgressIndicator(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentWidth(CenterHorizontally)
-                )
             }
+        }
+        
+        when {
             allItems.loadState.append is LoadState.Loading -> {
-                CircularProgressIndicator(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
+                CircularProgressIndicator()
             }
             allItems.loadState.refresh is LoadState.Error -> {
                 LaunchedEffect(key1 = true) {
